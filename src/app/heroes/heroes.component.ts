ef6aefe5f589415e43d3d96cf8b56d15ff33c59e
miltonmaleiane <miltonmaleiane1@gmail.com>
2023-02-01 14:11:50 +0200

    Using interfaces

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-
+  value:number =1.35
+hero: Hero={
+  name : "johny test",
+  id: 1,
+}
+ 
 }
